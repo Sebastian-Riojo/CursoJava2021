e9fe0b8c6aa498a76b571f7d9cba69566582540a
Sebastian-Riojo <77784374+Sebastian-Riojo@users.noreply.github.com>
2021-09-16 16:30:21 +0200

    Ejercicio2 y Ejercicio3

---
+++ CursoJava/src/modulo1/Ejercicio2.java
@@ -0,0 +1,17 @@
+package modulo1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Nombre\t\tApellido\t\tE-mail\t\t\t\tTeléfono");
+		System.out.println("-----\t\t--------\t\t------\t\t\t\t--------");
+		System.out.println("\n");
+		System.out.println("Sebastián\tRiojo\t\t\tsebastianriojo@gmail.com\t692134819");
+		System.out.println("Monsef\t\tBakhtaoui\t\tmonsef123@gmail.com\t\t617899876");
+		System.out.println("Iván\t\tAltares\t\t\tIvan123@gmail.com\t\t645324512");
+		
+	
+	}
+	
+}
---
+++ CursoJava/src/modulo1/Ejercicio3.java
@@ -0,0 +1,18 @@
+package modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tecla de escape\t\tSignificado");
+		System.out.println("\n");
+		System.out.println("\\n\t\t\tsignifica nueva línea");
+		System.out.println("\\t\t\t\tsignifica un tab de espacio");
+		System.out.println("\\\"\t\t\tes para poner “ (comillas dobles) dentro del texto por ejemplo\r\n"
+				+ "\t\t\t“Belencita”");
+		System.out.println("\n\\\\\t\t\tse utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\\'\t\t\tse utiliza para las ‘(comilla simple) para escribir por ejemplo ‘Princesita’");
+
+	}
+
+}
