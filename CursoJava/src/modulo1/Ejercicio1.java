3b92da332f2451aa100690b04cdce4512a52da53
Sebastian-Riojo <77784374+Sebastian-Riojo@users.noreply.github.com>
2021-09-16 15:53:42 +0200

    agregado de letras

--- CursoJava/src/modulo1/Ejercicio1.java
+++ CursoJava/src/modulo1/Ejercicio1.java
@@ -5,6 +5,10 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Hola
 		System.out.println("Hola Mundo");
+		System.out.println("\n\n\ntres lugares hacia abajo");
+		System.out.println("constante\t\t\"valor\"");
+		System.out.println("\nPI\t\t\t3.1216");
+		System.out.println("e\t\t\t22.71");
 
 	}
 
